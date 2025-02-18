0a497a37696a3f4305c53b2a4d33d1e984c6f0e9
MarioGotta <mario.lucio57@gmail.com>
2025-02-18 01:13:34 -0300

    Exercicios

---
+++ Exercicios/Exercicio85.py
@@ -0,0 +1,4 @@
+# Crie um programa onde o usuário possa digitar
+# 7 valores numéricos e os cadastre-os em uma lista única
+# que mantenha separados os valores pares e impares.]
+# No final mostre os valores pares e ímpares em ordem crescente.
